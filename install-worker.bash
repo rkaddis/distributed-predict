23e568b042eaede26eb35cb4d87f17874a7a2b7b
rkaddis <rkaddis@ltu.edu>
2025-04-21 13:48:35 -0400

    make service install optional

--- install-worker.bash
+++ install-worker.bash
@@ -2,7 +2,12 @@
 
 sudo echo
 pip install --break-system-packages -r requirements.txt worker
+input=""
+read -p "Install as a service? [Y/n]: " input
+if [[ input == "y" ]]
+then
 sudo ln -s distributed-worker.service /etc/systemd/system/
 sudo systemctl enable distributed-worker.service
 sudo systemctl daemon-reload
+fi
 echo "Installation complete. Reboot to start the worker service."
