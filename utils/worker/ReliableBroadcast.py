da254f4e92de0b38a6a1ebf2ce128fbbb98c15fa
rkaddis <rkaddis@ltu.edu>
2025-04-21 15:54:46 -0400

    hash video 5/?

--- utils/worker/ReliableBroadcast.py
+++ utils/worker/ReliableBroadcast.py
@@ -20,7 +20,7 @@ class RBInstance:
         self.hash_value = None
         
         if self.use_hash:
-            self.hash_value = sha256(initial_message.data.encode())
+            self.hash_value = sha256(initial_message.data.encode()).hexdigest()
 
         # send out your initial contents as an echo
         if self.use_hash:
@@ -64,7 +64,7 @@ class RBInstance:
 
             if max_count >= (n + f) // 2:
                 if self.use_hash:
-                    ready_message = RBMessage("ready", self.initial_message.subject, sha256(max_data.encode()))
+                    ready_message = RBMessage("ready", self.initial_message.subject, sha256(max_data.encode()).hexdigest())
                 else:
                     ready_message = RBMessage("ready", self.initial_message.subject, max_data)
                 self.send_all(ready_message)
@@ -75,7 +75,7 @@ class RBInstance:
 
             if max_count >= (2 * f + 1):
                 if self.use_hash:
-                    if sha256(max_data.encode()) == self.hash_value:
+                    if sha256(max_data.encode()).hexdigest() == self.hash_value:
                         accept_message = RBMessage("accepted", self.initial_message.subject, self.initial_message.data)
                         return accept_message
                     else:
